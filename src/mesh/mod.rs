bdb977cbb7bf0b4bb40af2e8bc775e95b264ab26
baldraven <elyaselaziz@gmail.com>
2025-01-21 16:14:32 +0100

    Partial attempt to reduce number of duplicate vertices

--- src/mesh/mod.rs
+++ src/mesh/mod.rs
@@ -1,28 +1,78 @@
 use honeycomb::core::cmap::CMap2;
 use honeycomb::core::prelude::{CMapBuilder, Vertex2};
-use honeycomb::render::App;
 use std::collections::HashMap;
 
 fn is_subset(sub: &[usize], sup: &[usize]) -> bool {
     sub.iter().all(|x| sup.contains(x))
 }
 
-/// Remove vertices that are subsets of others
-fn remove_subsets(vertices: &mut Vec<Vec<usize>>) {
-    let mut i = 0;
-    while i < vertices.len() {
-        let mut j = 0;
-        while j < vertices.len() {
-            if i != j && is_subset(&vertices[i], &vertices[j]) {
-                vertices.remove(i);
-                i = i.saturating_sub(1);
-                j = 0;
-            } else {
-                j += 1;
+// If some vertices are a subset of others, keep the one with the most colors
+// e.g., input [[[1, 2, 3], [1, 2, 3, 4]], [1,2,3]] outputs [[[1, 2, 3, 4]],[[1, 2, 3, 4]]]
+// input vec![vec![vec![1, 2, 4], vec![1, 2, 5]], vec![vec![1, 2, 6], vec![1, 2, 7]]] outputs vec![vec![vec![1, 2, 4], vec![1, 2, 5]], vec![vec![1, 2, 6], vec![1, 2, 7]]]
+// input vec![vec![vec![1, 2, 3], vec![1, 2, 3, 4]], vec![vec![1, 2, 5], vec![1, 2, 4, 5]]] outputs vec![vec![vec![1, 2, 3, 4]], vec![vec![1, 2, 4, 5]]]
+fn remove_subsets_quadratic(vertices: &mut Vec<Vec<Vec<usize>>>, vertex_map: &mut HashMap<Vec<usize>, (u32, u32)>) {
+    let mut max_vertices: HashMap<Vec<usize>, Vec<usize>> = HashMap::new();
+
+    // First pass: find all subsets and their corresponding maximal sets
+    for vertex_list in vertices.iter() {
+        for i in 0..vertex_list.len() {
+            for j in 0..vertex_list.len() {
+                if i != j && is_subset(&vertex_list[i], &vertex_list[j]) {
+                    let current_max = max_vertices
+                        .entry(vertex_list[i].clone())
+                        .or_insert_with(|| vertex_list[i].clone());
+                    if vertex_list[j].len() > current_max.len() {
+                        *current_max = vertex_list[j].clone();
+                    }
+                    // Remove the subset from vertex_map since it will be replaced
+                    vertex_map.remove(&vertex_list[i]);
+                }
             }
         }
-        i += 1;
     }
+
+    // Second pass: replace all subsets with their maximal sets
+    for vertex_list in vertices.iter_mut() {
+        let mut has_subset = false;
+        let mut max_vertex = vertex_list[0].clone();
+
+        for v in vertex_list.iter() {
+            if let Some(max) = max_vertices.get(v) {
+                has_subset = true;
+                if max.len() > max_vertex.len() {
+                    max_vertex = max.clone();
+                }
+                // Remove the subset from vertex_map
+                vertex_map.remove(v);
+            }
+        }
+
+        if has_subset {
+            *vertex_list = vec![max_vertex];
+        }
+    }
+}
+
+fn get_unique_colors(x: u32, y: u32, res: usize, grid: &[usize]) -> Vec<usize> {
+    [
+        (x.saturating_sub(1), y.saturating_sub(1)),
+        (x, y.saturating_sub(1)),
+        (x + 1, y.saturating_sub(1)),
+        (x.saturating_sub(1), y),
+        (x + 1, y),
+        (x.saturating_sub(1), y + 1),
+        (x, y + 1),
+        (x + 1, y + 1),
+    ]
+    .iter()
+    .map(|&(nx, ny)| {
+        if nx > 0 && ny > 0 && nx < res as u32 && ny < res as u32 {
+            grid[ny as usize * res + nx as usize]
+        } else {
+            0
+        }
+    })
+    .collect()
 }
 
 /// Extracts the vertices of the Voronoi cells from the pixel grid.
@@ -39,27 +89,7 @@ pub fn extract_voronoi_cell_vertices(
         let y = (idx / res) as u32;
 
         // Collect unique colors in the 8-neighborhood
-        // FIXME: A little problem with saturating_sub : lines and column 0 are considered as outside the boundary
-        let mut unique_colors: Vec<usize> = [
-            (x.saturating_sub(1), y.saturating_sub(1)),
-            (x, y.saturating_sub(1)),
-            (x + 1, y.saturating_sub(1)),
-            (x.saturating_sub(1), y),
-            (x + 1, y),
-            (x.saturating_sub(1), y + 1),
-            (x, y + 1),
-            (x + 1, y + 1),
-        ]
-        .iter()
-        .map(|&(nx, ny)| {
-            // Check if neighbor is in bounds or at boundary
-            if nx > 0 && ny > 0 && nx < res as u32 && ny < res as u32 {
-                grid[ny as usize * res + nx as usize]
-            } else {
-                0 // Use 0 to represent None/boundary
-            }
-        })
-        .collect();
+        let mut unique_colors = get_unique_colors(x, y, res, grid);
 
         unique_colors.sort();
         unique_colors.dedup();
@@ -175,7 +205,7 @@ pub fn sort_vertices_topologically(
 
 /// Generates a combinatorial map from a pixel grid by sewing darts between vertices.
 /// Each face in the map corresponds to a color region in the pixel grid.
-pub fn generate_mesh(pixels: &[usize], num_colors: usize) -> Result<(), &'static str> {
+pub fn generate_mesh(pixels: &[usize], num_colors: usize) -> Result<CMap2<f32>, &'static str>{
     let res = (pixels.len() as f64).sqrt() as usize;
     let mut color_vertices: Vec<Vec<Vec<usize>>> = vec![Vec::new(); num_colors];
     let mut vertex_map: HashMap<Vec<usize>, (u32, u32)> = HashMap::new();
@@ -188,13 +218,13 @@ pub fn generate_mesh(pixels: &[usize], num_colors: usize) -> Result<(), &'static
 
     let mut dart_id = 1;
 
+    remove_subsets_quadratic(&mut color_vertices, &mut vertex_map); // We might want to change the logic here if we see significant benefits thanks to profiling, having higher resolution could work
+
     // Process each face (color region)
     for face_vertices in color_vertices.iter_mut() {
         if face_vertices.len() < 3 {
             continue;
         }
-
-        remove_subsets(face_vertices);
 
         // Sort vertices using topological information instead of angles
         sort_vertices_topologically(face_vertices, &vertex_map);
@@ -235,65 +265,5 @@ pub fn generate_mesh(pixels: &[usize], num_colors: usize) -> Result<(), &'static
         map.force_sew::<1>(dart_id - 1, dart_id - face_vertices.len() as u32);
     }
 
-    //map.iter_faces().collect()
-
-    // Visualize the result
-    let mut render_app = App::default();
-    render_app.add_capture(&map);
-    render_app.run();
-
-    Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_extract_voronoi_cell_vertices() {
-        let pixels = vec![
-            2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 5, 5, 5, 5, 5, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 5, 5,
-            5, 5, 5, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 5, 5, 5, 5, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1,
-            1, 1, 5, 5, 5, 5, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 5, 5, 5, 5, 2, 2, 2, 1, 1, 1, 1,
-            1, 1, 1, 1, 1, 3, 3, 3, 3, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 3, 3, 3, 2, 2, 1, 1,
-            1, 1, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 2, 2, 4, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 4,
-            4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3, 3,
-            3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-            4, 4, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4,
-            4, 4, 4, 4, 4, 4, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3,
-        ];
-        let num_colors = 5;
-        let res = (pixels.len() as f64).sqrt() as usize;
-        let mut color_vertices: Vec<Vec<Vec<usize>>> = vec![Vec::new(); num_colors];
-        let mut vertex_map: HashMap<Vec<usize>, (u32, u32)> = HashMap::new();
-        extract_voronoi_cell_vertices(&pixels, res, &mut color_vertices, &mut vertex_map);
-
-        let expected_color_vertices = vec![
-            vec![
-                vec![0, 1, 2],
-                vec![0, 1, 5],
-                vec![1, 3, 5],
-                vec![1, 2, 4],
-                vec![1, 3, 4],
-            ],
-            vec![vec![0, 1, 2], vec![0, 1, 2, 4], vec![0, 2, 4]],
-            vec![vec![1, 3, 5], vec![0, 3, 5], vec![1, 3, 4], vec![0, 3, 4]],
-            vec![vec![1, 2, 4], vec![1, 3, 4], vec![0, 2, 4], vec![0, 3, 4]],
-            vec![vec![0, 1, 5], vec![1, 3, 5], vec![0, 3, 5]],
-        ];
-
-        let mut expected_vertex_map = HashMap::new();
-        expected_vertex_map.insert(vec![1, 3, 5], (11, 4));
-        expected_vertex_map.insert(vec![1, 2, 4], (2, 7));
-        expected_vertex_map.insert(vec![0, 3, 5], (15, 4));
-        expected_vertex_map.insert(vec![1, 3, 4], (9, 7));
-        expected_vertex_map.insert(vec![0, 1, 2], (4, 0));
-        expected_vertex_map.insert(vec![0, 1, 2, 4], (1, 7));
-        expected_vertex_map.insert(vec![0, 3, 4], (15, 14));
-        expected_vertex_map.insert(vec![0, 2, 4], (0, 8));
-        expected_vertex_map.insert(vec![0, 1, 5], (10, 0));
-
-        assert_eq!(color_vertices, expected_color_vertices);
-        assert_eq!(vertex_map, expected_vertex_map);
-    }
+    Ok(map)
 }
